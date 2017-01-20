7641e2db0432d4405429f1b3be9103426056979b
Clapinton <eric.olivetree@gmail.com>
2017-01-19 18:54:35 -0800

    Form correctly POSTing

--- js/contact_me.js
+++ js/contact_me.js
@@ -2,8 +2,22 @@
 
 function handleSubmit(e) {
     e.preventDefault();
+    console.log(e.target[1].value);
+
+    dataBody = {
+        message: {
+            from_email: e.target[0].value,
+            body: e.target[1].value,
+        }
+    };
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:3000/api/messages',
+        data: dataBody,
+        success: function(res) {console.log('success: ',res)},
+        error: function(err) {console.log('error: ', err)}
+    });
     alert("submitted!");
-    console.log(e);
 }
 
 $("#contact form").submit(handleSubmit);
