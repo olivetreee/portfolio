9c8103a7aa238992f461b53f6406a04e208c9578
Clapinton <eric.olivetree@gmail.com>
2017-01-22 19:52:29 -0800

    Add reCAPTCHA. Form working on local

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,31 +1,51 @@
 // Contact Form Scripts
 
-function success(res) {
-    const successMessage = `
-    <p class='success-message'>
-    Message received! I'll get back to you asap. Thanks!
-    </p>
-    `;
-    $(".contact-form form").remove();
-    $(".contact-form").append(successMessage);
-}
+$(".contact-form").submit(handleSubmit);
+$(".contact-form").keyup(handleKeypress);
 
 function handleSubmit(e) {
     e.preventDefault();
+    console.log(e.target[2].value);
     
-    const from_email = e.target[0].value;
-    const validEmail = checkEmailPresence(from_email);
+    const fromEmail = e.target[0].value;
+    const validEmail = checkEmailPresence(fromEmail);
 
-    const body = e.target[1].value
+    const body = e.target[1].value;
     const validMessage = checkBodyPresence(body);
 
-    if (validEmail && validMessage) {
+    const captchaKey = e.target[2].value;
+    const validCaptcha = checkCaptchaPresence(captchaKey);
+
+    if (validEmail && validMessage && validCaptcha) {
         dataBody = {
             message: {
-                from_email: e.target[0].value,
-                body: e.target[1].value,
+                from_email: fromEmail,
+                body: body,
+                captcha_key: captchaKey
             }
         };
+
+        function success(res) {
+            const successMessage = `
+            <h3 class='success-message'>
+            Message received! I'll get back to you asap. Thanks!
+            </h3>
+            `;
+            $(".contact-form form").remove();
+            $(".contact-form").append(successMessage);
+            console.log(res);
+        }
+
+        function error(res) {
+            const errorMessage = `
+            <h3 class='error-message'>
+            Oops... something went wrong. Mind trying again?
+            </h3>
+            `;
+            $(".contact-form").prepend(errorMessage);
+            console.log(res);
+        }
+
         $.ajax({
             type: 'post',
             url: 'http://localhost:3000/api/messages',
@@ -34,13 +54,11 @@ function handleSubmit(e) {
             error: function(err) {console.log('error: ', err)}
         });
     }
-
 }
 
-function checkEmailPresence(from_email) {
-    if (from_email === "") {
-        const alertElement = $(".email-alert");
-        if (alertElement.length === 0) $(".email-input input").addClass('input-danger');
+function checkEmailPresence(fromEmail) {
+    if (!(/.+@.+\..{2,}/.test(fromEmail))) { //Expects an e-mail in the form a@a.co
+        $(".email-input input").addClass('input-danger');
         return false;
     }
     $(".email-input input").removeClass("input-danger");
@@ -49,13 +67,26 @@ function checkEmailPresence(from_email) {
 
 function checkBodyPresence(body) {
     if (body === "") {
-        const alertElement = $(".body-alert");
-        if (alertElement.length === 0) $(".body-input textarea").addClass('input-danger');
+        $(".body-input textarea").addClass('input-danger');
         return false;
     }
     $(".body-input textarea").removeClass("input-danger");
     return true;
 }
 
+function checkCaptchaPresence(captchaKey) {
+    if (captchaKey === "") {
+        $(".captcha-submit .g-recaptcha").addClass('input-danger');
+        return false;
+    }
+    $(".captcha-submit .g-recaptcha").removeClass('input-danger');
+    return true;
+}
 
-$("#contact form").submit(handleSubmit);
+function handleKeypress(e) {
+    if (e.target.id === "email") {
+        checkEmailPresence(e.target.value);
+    } else {
+        checkBodyPresence(e.target.value);
+    }
+}
